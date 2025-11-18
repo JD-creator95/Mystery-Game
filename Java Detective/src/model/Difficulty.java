30e9fa73a3bb4272abe4a87bb52b446ee84a29a9
Juan De Bruyn <juan.debruyn@umuzi.org>
2025-11-18 08:05:08 +0200

    Delete Java Detective directory

--- Java Detective/src/model/Difficulty.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public enum Difficulty {
-
-    EASY, MEDIUM, HARD
-
-}
-// This enum represents the difficulty levels of the game.
-// It can be used to adjust the game's complexity based on player preference.
